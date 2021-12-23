9ac54b2107a11a505267f01ee4f2252a545ac9af
coderachel <3079431727@qq.com>
2021-12-23 23:36:33 +0800

    component 初始化流程

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,41 @@
+export function createComponentInstance(vnode: any) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  };
+  return component;
+}
+
+export function setupComponent(instance) {
+  // TODO
+  // initProps()
+  // initSlots()
+  setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance: any) {
+  const Component = instance.type;
+  const { setup } = Component;
+
+  if (setup) {
+    const setupResult = setup();
+    handleSetupResult(instance, setupResult);
+  }
+}
+
+function handleSetupResult(instance, setupResult: any) {
+  if (typeof setupResult === "object") {
+    instance.setupState = setupResult;
+  }
+
+  finishComponentSetup(instance);
+}
+
+// 最终肯定是要返回一个render函数
+function finishComponentSetup(instance: any) {
+  const Component = instance.type;
+
+  if (!Component.render) {
+    instance.render = Component.render;
+  }
+}
---
+++ src/runtime-core/renderer.ts
@@ -0,0 +1,25 @@
+import { createComponentInstance, setupComponent } from "./component";
+
+export function render(vnode, container) {
+  patch(vnode, container);
+}
+
+function patch(vnode, container) {
+  processComponent(vnode, container);
+}
+
+function processComponent(vnode: any, container: any) {
+  mountComponent(vnode, container);
+}
+
+function mountComponent(vnode: any, container: any) {
+  const instance = createComponentInstance(vnode);
+
+  setupComponent(instance);
+  setupRenderEffect(instance, container);
+}
+
+function setupRenderEffect(instance: any, container) {
+  const subTree = instance.children;
+  patch(subTree, container);
+}
